2f62d3eb7fc4fb058306f01e3a572aa43d92e038
patrick schubert <pschub@uw.edu>
2013-04-16 15:40:30 -0700

    Added GPL copyright junk

--- lcdDriver.c
+++ lcdDriver.c
@@ -1,7 +1,22 @@
 /*
- * LCD driver FOR ATMEGA328p
- *
- *
+    LCD Driver for ATMEGA 329p 
+    Copyright (C) 2013 Patrick Schubert 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
  * Uses PORTD6 for R/nW pin
  *      PORTC[0:3] for data pins
  *      PORTC[4] for RS pin
